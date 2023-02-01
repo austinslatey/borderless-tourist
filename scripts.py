d17f4cd6e4ebd1e1033ef362cab506369b4fdb4e
Trappin Recidivism <austinslater58@gmail.com>
2023-01-31 19:48:03 -0600

    Added logic to find traveler destinations and convert to internal data commented out print statement on line 16, no longer needed

--- scripts.py
+++ scripts.py
@@ -12,4 +12,13 @@ def get_destination_index(destination):
     destination_index = destinations.index(destination)
     return destination_index
 
-print(get_destination_index("Los Angeles, USA"))
+# Tests function above by passing string as parameter
+# print(get_destination_index("Los Angeles, USA"))
+
+def get_traveler_location(traveler):
+    traveler_destination = traveler[1]
+    traveler_destination_index = get_destination_index(traveler_destination)
+    return traveler_destination_index
+
+test_destination_index = get_traveler_location(test_traveler)
+print(test_destination_index)
