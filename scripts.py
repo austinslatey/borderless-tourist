23288a7ee8a257d4203ed387fb5ad9db8bf7c97b
Trappin Recidivism <austinslater58@gmail.com>
2023-01-31 20:08:22 -0600

    Ability to append an attraction to the attractions list successful

--- scripts.py
+++ scripts.py
@@ -22,3 +22,23 @@ def get_traveler_location(traveler):
 
 test_destination_index = get_traveler_location(test_traveler)
 print(test_destination_index)
+
+attractions = []
+# append empty lists to attractions for each destination 
+for destination in destinations:
+    attractions.append([])
+
+print(attractions)
+
+# Add attractions to empty list
+def add_attraction(destination, attraction):
+    try:
+        destination_index = get_destination_index(destination)
+        attractions_for_destinations = attractions[destination_index].append(attraction)
+        return attractions_for_destinations
+    except SyntaxError:
+        return SyntaxError
+
+# Call function and then print updated list
+add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
+print(attractions)
